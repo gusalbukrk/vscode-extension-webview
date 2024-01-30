bd58741d8bb2582bc0cd08b2d836bf66b70332d3
gusalbukrk <gusalbukrk@gmail.com>
2024-01-30 00:09:53 -0300

    webviews visibility & moving

--- src/extension.ts
+++ src/extension.ts
@@ -34,41 +34,69 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	context.subscriptions.push(disposable2);
 
+	// https://code.visualstudio.com/api/extension-guides/webview#visibility-and-moving
+	// track the current panel with a webview
+	// make impossible to open multiple panels of same type at once
+	let currentPanel: vscode.WebviewPanel | undefined = undefined;
+
 	// https://code.visualstudio.com/api/extension-guides/webview#webviews-api-basics
 	let disposable3 = vscode.commands.registerCommand('catCoding.start', () => {
-      // Create and show a new webview
-      const panel = vscode.window.createWebviewPanel(
-        'catCoding', // Identifies the type of the webview. Used internally
-        'Cat Coding', // Title of the panel displayed to the user
-				vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-        {} // Webview options. More on these later.
-      );
+		// the effects of this will be visible only when the editor is splitted in multiple columns,
+		// if command is called when already there's a currentPanel but it's in a different column
+		// currentPanel will be moved to the column where the command was called;
+		// if instead `currentPanel.reveal()` were to be called without an argument,
+		// currentPanel wouldn't be moved to current column
+		// instead the focus would be moved to the column where the panel is
+      const columnToShowIn = vscode.window.activeTextEditor
+        ? vscode.window.activeTextEditor.viewColumn
+        : undefined;
 
-			// https://code.visualstudio.com/api/extension-guides/webview#updating-webview-content
-      let iteration = 0;
-      const updateWebview = () => { // will keep running even after panel is closed
-				console.log('from `updateWebview`');
-        panel.webview.html = getWebviewContent(iteration++ % 2 ? 'compiling' : 'coding');
-      };
-			//
-      // Set initial content
-      updateWebview();
-			//
-      // And schedule updates to the content every second
-      const interval = setInterval(updateWebview, 1000);
+				if (currentPanel) { // if we already have a panel, show it in the target column
+					currentPanel.reveal(columnToShowIn);
+				} else { // otherwise, create a new panel
+					// Create and show a new webview
+					currentPanel = vscode.window.createWebviewPanel(
+						'catCoding', // Identifies the type of the webview. Used internally
+						'Cat Coding', // Title of the panel displayed to the user
+						vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+						{} // Webview options. More on these later.
+					);
 
-			// https://code.visualstudio.com/api/extension-guides/webview#lifecycle
-      panel.onDidDispose(
-        () => {
-          // When the panel is closed, cancel any future updates to the webview content
-          clearInterval(interval);
-        },
-        null,
-        context.subscriptions
-      );
+					// https://code.visualstudio.com/api/extension-guides/webview#updating-webview-content
+					let iteration = 0;
+					const updateWebview = () => { // will keep running even after panel is closed
+						console.log('from `updateWebview`');
+						currentPanel!.webview.html = getWebviewContent(iteration++ % 2 ? 'compiling' : 'coding');
+					};
+					//
+					// Set initial content
+					updateWebview();
+					//
+					// And schedule updates to the content every second
+					const interval = setInterval(updateWebview, 1000);
 
-			// it's also possible to programmatically close webviews by calling dispose() on them
-			// panel.dispose();
+					// https://code.visualstudio.com/api/extension-guides/webview#lifecycle
+					currentPanel.onDidDispose(
+						() => {
+							// When the panel is closed, cancel any future updates to the webview content
+							clearInterval(interval);
+							currentPanel = undefined;
+						},
+						null,
+						context.subscriptions
+					);
+
+					// it's also possible to programmatically close webviews by calling dispose() on them
+					// panel.dispose();
+
+					// currentPanel.onDidChangeViewState(
+					// 	e => {
+					// 		console.log('panel visibility changed or it was move from a column to another');
+					// 	},
+					// 	null,
+					// 	context.subscriptions
+					// );
+				}
     });
 
 	context.subscriptions.push(disposable3);
